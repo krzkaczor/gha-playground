1811680504e67514f7c1d873c28afa4d53543729
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-05 23:33:52 +0100

    trigger playground

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core'
 export function action() {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
+  console.log(`Hello ${nameToGreet}!!!`)
   const time = new Date().toTimeString()
   core.setOutput('time', time)
 }
