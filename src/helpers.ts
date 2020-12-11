f5a39289b18ecf4d60e96727895b24e0a0e1f45c
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-11 20:22:07 +0100

    Check permissions before running. add +1 on success

--- src/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { context } from '@actions/github'
-
-type Context = typeof context
-
-export function getCommentBody(ctx: Context): string | undefined {
-  const body = ctx.payload.comment?.body
-
-  return body
-}
