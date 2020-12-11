3e568b977a231c0831fe0bd8e22210eef9c8b0d8
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-11 21:08:58 +0100

    fix

--- src/github/octokit.ts
+++ src/github/octokit.ts
@@ -5,7 +5,7 @@ export type Octokit = ReturnType<typeof nativeGetOctokit>
 export type GithubContext = typeof context
 
 export function getOctokit(): Octokit {
-  const ghToken = process.env.GITHUB_TOKEN
+  const ghToken = process.env.INPUT_GITHUB_TOKEN
   assert(ghToken, 'GITHUB_TOKEN not found. Did you forgot to pass it as env?')
 
   return nativeGetOctokit(ghToken)
