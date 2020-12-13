d0f2b283bbbc0ce8ad9fd3ab6e9c94abb619d7f0
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-13 17:33:49 +0100

    Copy example to the tmp dir and run tests there

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,11 +1,14 @@
 import { getOctokit } from '@actions/github'
-import { mockFn } from 'earljs'
+import { expect, mockFn } from 'earljs'
+import { readFileSync } from 'fs'
+import { copySync } from 'fs-extra'
 import { join } from 'path'
+import tmp from 'tmp'
 
 import { action } from '../src/action'
 import { Exec } from '../src/types'
 import { Context } from './helpers/GithubCtxFromEvent'
-import { nockTest } from './nockTest'
+import { nockTest } from './helpers/nockTest'
 
 const event = require('./fixtures/validEvent.json')
 
@@ -13,12 +16,22 @@ describe('integration', () => {
   it(
     'works',
     nockTest(async () => {
+      // prepare directory with dummy allcontributors setup
+      const workdirPath = tmp.tmpNameSync()
+      copySync(join(__dirname, './fixtures/example'), workdirPath)
+
       const octokit = getOctokit('NOT_EXISTING_TOKEN')
-      const cwd = join(__dirname, '../example')
       const execMock = mockFn<Exec>().resolvesTo(0)
       const ctx = new Context(event)
 
-      await action({ cwd, exec: execMock, octokit, ctx })
+      await action({ cwd: workdirPath, exec: execMock, octokit, ctx })
+
+      // assert that a new contributor was added and readme regenerated
+      const allContributorsCfg = JSON.parse(readFileSync(join(workdirPath, '.all-contributorsrc'), 'utf-8'))
+      const exampleReadme = readFileSync(join(workdirPath, 'README.md'), 'utf8')
+
+      expect(allContributorsCfg.contributors.length).toEqual(1)
+      expect(exampleReadme).toEqual(expect.stringMatching('monalisa octocat'))
     }),
   )
 })
